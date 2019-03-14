11796030b1c797860ba3e402f573d4c2bc6f72e2
Pat Evans <patrickevans98@gmail.com>
2019-03-14 00:35:02 +0000

    Added rook possible moves - including ensuring it cannot jump over any other pieces

--- Chess Engine/src/chessengine/Rook.java
+++ Chess Engine/src/chessengine/Rook.java
@@ -7,6 +7,30 @@ public class Rook extends Piece{
 		this.yPosition=startY;
 		this.colour=colour;
 	}
+	@Override
+    public void possibleMoves() {
+		
+		//These are the up down possible moves
+		for(int i=yPosition;i<8;i++) {
+			if(Main.occupied(xPosition,i)) break;
+			this.addToPossibleLocations(xPosition,i);
+		}
+		for(int i=yPosition;i<8;i--) {
+			if(Main.occupied(xPosition,i)) break;
+			this.addToPossibleLocations(xPosition,i);
+		}
+		
+		//These are the left right possible moves
+		for(int i=xPosition;i<8;i++) {
+			if(Main.occupied(i,yPosition)) break;
+	    	this.addToPossibleLocations(i,yPosition);
+		}
+		for(int i=xPosition;i<8;i--) {
+			if(Main.occupied(i,yPosition)) break;
+	    	this.addToPossibleLocations(i,yPosition);
+		}
+		 	
+    }
 	public String toString() {
 		return "Rook " + super.toString();
 	}
